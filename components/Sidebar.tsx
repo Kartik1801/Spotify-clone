8afd29e7dbaae6db7855dc2d975ae4ee5d000a03
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-07 23:54:14 +0530

    layout for navigation and player

---
+++ components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+const Sidebar = () => {
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      js
+    </Box>
+  );
+};
+
+export default Sidebar;
