c74e33c8f672fb30cef6f5c902cfbb7e0507c233
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-13 16:44:26 +0530

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import PlayerLayout from "../components/PlayerLayot";
+import PlayerLayout from "../components/PlayerLayout";
 import "reset-css";
 import "../styles/globals.css";
 
