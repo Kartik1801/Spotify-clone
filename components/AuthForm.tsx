9069b472f1b60cc4a587c699ba3318e4f5a2b71c
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-14 22:45:44 +0530

    AuthForm initail

---
+++ components/AuthForm.tsx
@@ -0,0 +1,23 @@
+import { Box, Flex, Input, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { useSWRConfig } from "swr";
+import { auth } from "../lib/mutation";
+
+const AuthForm: FC<{ mode: string }> = ({ mode }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  return (
+    <Box height="100vh" width="100vw" bg="black" color="white">
+      <Flex justify="center" align="center" height="100px">
+        hello
+      </Flex>
+      <Flex justify="center" align="center" height="calc(100vh-100px)" />
+    </Box>
+  );
+};
+
+export default AuthForm;
