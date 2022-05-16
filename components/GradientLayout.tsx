a61efa49f69bd8197e470370eede420f1b44399a
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-17 00:24:31 +0530

    💻 UI Improvements.

--- components/GradientLayout.tsx
+++ components/GradientLayout.tsx
@@ -13,9 +13,9 @@ const GradientLayout = ({
 
     return (
         <Box height="100%" overflowY="auto" bgGradient={`linear(${color}.900 0%, ${color}.800 5%, ${color}.900 20%, rgba(0,0,0,0.9) 45%)`} >
-            <Flex paddingTop="40px" paddingBottom="30px" paddingX="30px" align="end" >
+            <Flex paddingTop="20px" paddingBottom="30px" paddingX="30px" align="end" >
                 <Box paddingY="30px" paddingX="5px" >
-                    <Image boxSize="225px" boxShadow="dark-lg" src={image} borderRadius={roundImage ? "100%" : "3px"} />
+                    <Image boxSize="220px" boxShadow="dark-lg" src={image} borderRadius={roundImage ? "100%" : "3px"} />
                 </Box>
                 <Box paddingX="25px" paddingY="20px" color="white" lineHeight="65px">
                      <Text casing="uppercase" fontSize="12" fontWeight="700">
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,24 +5,24 @@ import prisma from "../lib/prisma";
 
 const Home = ({artists}) => {
   return <GradientLayout roundImage={true} image={"https://i.pinimg.com/originals/16/b8/26/16b826ca6cd8935c59a086bed5419640.jpg"} subtitle={"profile"} color={"red"} title={"KD"} description="15 Followers &#8226; 3 Playlists">
-    <Box color="white">
-        <Box marginBottom="30px">
-          <Text fontSize="xl" fontWeight="bold">
-            Top Artists this month
+    <Box paddingLeft="5px" color="white">
+        <Box marginBottom="10px">
+          <Text fontSize="24px" fontWeight="bold">
+            Top artists this month
           </Text>
-          <Text fontSize="sm" color="gray.500" >
+          <Text paddingLeft="2px" fontSize="sm" color="gray.500" >
             Only visible to you.
           </Text>
         </Box>
-        <Flex>
+        <Flex overflowX="auto">
           {
             artists.map(artist => (
-              <Box padding="10px" width="12%" >
-                <Box bg="#212121" width="95%" borderRadius="4px" padding="15px" >
-                  <Image src="https://placekitten.com/300/300" borderRadius="100%"  />   
-                  <Box marginTop="10px">
-                    <Text fontSize="large">{artist.name}</Text>
-                    <Text>Artist </Text>
+              <Box padding="10px" width="192.8px" height="270.8px" >
+                <Box bg="#212121" width="100%" height="100%" borderRadius="4px" padding="15px" >
+                  <Image src="https://placekitten.com/300/300" boxShadow="dark-lg" borderRadius="100%"  />   
+                  <Box marginTop="20px">
+                    <Text fontSize="16px" fontWeight="700">{artist.name}</Text>
+                    <Text fontSize="15px" fontWeight="400" color="gray.500">Artist </Text>
                   </Box>             
                 </Box>
               </Box>
