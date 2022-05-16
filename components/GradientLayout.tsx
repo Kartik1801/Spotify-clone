e8177d99e304bd9fd2567584ddec0dbb2e624405
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-16 20:29:44 +0530

    updated

--- components/GradientLayout.tsx
+++ components/GradientLayout.tsx
@@ -14,10 +14,23 @@ const GradientLayout = ({
     return (
         <Box height="100%" overflowY="auto" bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.9) 75%)`} >
             <Flex bg={`${color}.600`} padding="40px" align="end" >
-                hello
+                <Box padding="20px" >
+                    <Image boxSize="160px" boxShadow="2xl" src={image} borderRadius={roundImage ? "100%" : "3px"} />
+                </Box>
+                <Box padding="20px" color="white" lineHeight="50px">
+                     <Text casing="uppercase" fontSize="x-small" fontWeight="bold">
+                         {subtitle}
+                     </Text>
+                     <Text fontSize="6xl" >
+                        {title}
+                     </Text>
+                    <Text fontSize="sm">
+                    {description}
+                    </Text>
+                </Box>
             </Flex>
         </Box>
     )
 }
 
-export default GradientLayout
+export default GradientLayout 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import GradientLayout from "../components/GradientLayout";
 
 const Home = () => {
-  return <GradientLayout color={"red"} >
+  return <GradientLayout roundImage={true} image={"https://dl.dropboxusercontent.com/s/bgiv0ssz3xpotz9/peep.png?dl=0"} subtitle={"profile"} color={"red"} title={"KD"} description="15 Followers 3 Playlists">
     <div> 
       home
     </div>
