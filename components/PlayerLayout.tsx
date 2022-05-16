35fba80be65f53f4daa7c3ed33ec5260672ce3cd
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-16 20:04:36 +0530

    initial layout fixes

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -7,7 +7,7 @@ const PlayerLayout = ({ children }) => {
       <Box position="absolute" top="0" width="250px" left="0">
         <Sidebar />
       </Box>
-      <Box marginLeft="250px" marginBottom="100px">
+      <Box marginLeft="250px" height="calc(100vh - 100px)" marginBottom="100px">
         {children}
       </Box>
       <Box position="absolute" left="0" bottom="0">
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
+import GradientLayout from "../components/GradientLayout";
+
 const Home = () => {
-  return <div>Home</div>;
+  return <GradientLayout color={"red"} >
+    <div> 
+      home
+    </div>
+  </GradientLayout>;
 };
 export default Home;
