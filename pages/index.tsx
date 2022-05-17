e91023ba5bdedf1fbaaaceb447799ba0aa28ab16
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-17 23:20:47 +0530

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/react";
+import { Artist, Song } from "@prisma/client";
+import { FC } from "react";
 import GradientLayout from "../components/GradientLayout";
 import prisma from "../lib/prisma";
 
-const Home = ({artists}) => {
+interface HomeProps {
+  artists: Artist[];
+  songs: Song[];
+}
+
+const Home:FC<HomeProps> = ({artists, songs}) => {
   return <GradientLayout roundImage={true} image={"https://i.pinimg.com/originals/16/b8/26/16b826ca6cd8935c59a086bed5419640.jpg"} subtitle={"profile"} color={"red"} title={"KD"} description="15 Followers &#8226; 3 Playlists">
     <Box paddingLeft="5px" color="white">
         <Box marginBottom="10px">
           <Text fontSize="24px" fontWeight="bold">
             Top artists this month
           </Text>
-          <Text paddingLeft="2px" fontSize="sm" color="gray.500" >
+          <Text paddingLeft="2px" fontSize="sm" color="gray.500" fontWeight="500" >
             Only visible to you.
           </Text>
         </Box>
@@ -22,7 +29,7 @@ const Home = ({artists}) => {
                   <Image src="https://placekitten.com/300/300" boxShadow="dark-lg" borderRadius="100%"  />   
                   <Box marginTop="20px">
                     <Text fontSize="16px" fontWeight="700">{artist.name}</Text>
-                    <Text fontSize="15px" fontWeight="400" color="gray.500">Artist </Text>
+                    <Text fontSize="15px" fontWeight="500" color="gray.500">Artist </Text>
                   </Box>             
                 </Box>
               </Box>
@@ -33,19 +40,26 @@ const Home = ({artists}) => {
           <Text fontSize="24px" fontWeight="bold">
             Top tracks this month
           </Text>
-          <Text paddingLeft="2px" fontSize="sm" color="gray.500" >
+          <Text paddingLeft="2px" fontSize="sm" color="gray.500" fontWeight="500" >
             Only visible to you.
           </Text>
         </Box>
+        <Flex overflowY="auto">
+          {
+            songs.map((song: Song)=>(song.name))            
+          }
+        </Flex>
     </Box>
   </GradientLayout>;
 };
 
 export const getServerSideProps = async () => {
-  const artists = await prisma.artist.findMany({});
+  const artists: Artist[] = await prisma.artist.findMany({});
+  const songs: Song[] = await prisma.song.findMany({})
   return {
     props: {
-      artists
+      artists,
+      songs
     },
   }
 }
