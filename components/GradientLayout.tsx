06d9a8d311c4d123132775a18cc0c1c6c0bf3fbb
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-18 21:17:04 +0530

    updated

--- components/GradientLayout.tsx
+++ components/GradientLayout.tsx
@@ -1,38 +1,47 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
-import { Image } from  "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 
 const GradientLayout = ({
-    color,
-    children,
-    image,
-    subtitle,
-    title,
-    description,
-    roundImage
+  color,
+  children,
+  image,
+  subtitle,
+  title,
+  description,
+  roundImage,
 }) => {
-    return (
-        <Box height="100%" overflowY="auto" bgGradient={`linear(${color}.900 0%, ${color}.800 5%, ${color}.900 20%, rgba(0,0,0,0.9) 45%)`} >
-            <Flex paddingTop="20px" paddingBottom="30px" paddingX="30px" align="end" >
-                <Box paddingY="30px" paddingX="5px" >
-                    <Image boxSize="220px" boxShadow="dark-lg" src={image} borderRadius={roundImage ? "100%" : "3px"} />
-                </Box>
-                <Box paddingX="25px" paddingY="20px" color="white" lineHeight="65px">
-                     <Text casing="uppercase" fontSize="12" fontWeight="700">
-                        {subtitle}
-                     </Text>
-                     <Text fontSize="94" fontWeight="700">
-                        {title}
-                     </Text>
-                    <Text marginTop="3px" fontSize="14" fontWeight="600">
-                        {description}
-                    </Text>
-                </Box>
-            </Flex>
-            <Box paddingY="10px" paddingX="50px">
-                {children}
-            </Box>
+  return (
+    <Box
+      height="100%"
+      overflowY="auto"
+      bgGradient={`linear(${color}.900 0%, ${color}.800 5%, ${color}.900 20%, rgba(0,0,0,0.9) 45%)`}
+    >
+      <Flex paddingTop="20px" paddingBottom="30px" paddingX="30px" align="end">
+        <Box paddingY="30px" paddingX="5px">
+          <Image
+            boxSize="220px"
+            boxShadow="dark-lg"
+            src={image}
+            borderRadius={roundImage ? "100%" : "3px"}
+          />
         </Box>
-    )
-}
+        <Box paddingX="25px" paddingY="20px" color="white" lineHeight="65px">
+          <Text casing="uppercase" fontSize="12" fontWeight="700">
+            {subtitle}
+          </Text>
+          <Text fontSize="94" fontWeight="700">
+            {title}
+          </Text>
+          <Text marginTop="3px" fontSize="14" fontWeight="600">
+            {description}
+          </Text>
+        </Box>
+      </Flex>
+      <Box paddingY="10px" paddingX="50px">
+        {children}
+      </Box>
+    </Box>
+  );
+};
 
-export default GradientLayout 
+export default GradientLayout;
