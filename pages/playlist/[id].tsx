6e916832f2d8c6d547c10c9e60cd9cb0cfb2e6ab
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-20 22:46:22 +0530

    updated

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,8 +1,69 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Playlist as PlaylistModel } from "@prisma/client";
+import { FC } from "react";
+import {
+  MdPlayCircleFilled,
+  MdFavorite,
+  MdFavoriteBorder,
+} from "react-icons/md";
+import GradientLayout from "../../components/GradientLayout";
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 
-const Playlist = ({ playlist }) => {
-  return <div>{playlist.name}</div>;
+interface IPlaylist {
+  playlist: PlaylistModel;
+}
+
+const getBGColor = (id) => {
+  const color = [
+    "red",
+    "green",
+    "blue",
+    "orange",
+    "purple",
+    "gray",
+    "teal",
+    "yellow",
+  ];
+  return color[id - 1] || color[Math.floor(Math.random() * color.length)];
+};
+
+const Playlist: FC<IPlaylist> = ({ playlist }) => {
+  const color = getBGColor(playlist.id);
+
+  return (
+    <GradientLayout
+      color={color}
+      title={playlist.name}
+      description="Description"
+      roundImage={false}
+      image="https://placekitten.com/300/300"
+      subtitle="playlist"
+    >
+      <Box
+        width="100%"
+        height="100%"
+        paddingX={"15px"}
+        bgColor={"rgba(0,0,0,0.1)"}
+      >
+        <Flex padding="10px">
+          <Text height="65px" fontSize="65px" color="lightgreen">
+            <MdPlayCircleFilled />
+          </Text>
+          <Text
+            height="60px"
+            paddingY="15px"
+            paddingX="15px"
+            align="center"
+            fontSize="40px"
+            color="lightgreen"
+          >
+            <MdFavorite />
+          </Text>
+        </Flex>
+      </Box>
+    </GradientLayout>
+  );
 };
 
 export const getServerSideProps = async ({ query, req }) => {
