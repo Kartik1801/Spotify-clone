f8910e69af79f5e4a7b394a0640c4a9a7a661d5f
kartik1801 <kartikdhawan1801@gmail.com>
2022-05-21 19:54:36 +0530

    updated

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,5 +1,5 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
-import { Playlist as PlaylistModel } from "@prisma/client";
+import { Playlist as PlaylistModel, User } from "@prisma/client";
 import { FC } from "react";
 import {
   MdPlayCircleFilled,
@@ -12,6 +12,7 @@ import prisma from "../../lib/prisma";
 
 interface IPlaylist {
   playlist: PlaylistModel;
+  user: User;
 }
 
 const getBGColor = (id) => {
@@ -28,7 +29,7 @@ const getBGColor = (id) => {
   return color[id - 1] || color[Math.floor(Math.random() * color.length)];
 };
 
-const Playlist: FC<IPlaylist> = ({ playlist }) => {
+const Playlist: FC<IPlaylist> = ({ playlist, user }) => {
   const color = getBGColor(playlist.id);
 
   return (
@@ -40,12 +41,7 @@ const Playlist: FC<IPlaylist> = ({ playlist }) => {
       image="https://placekitten.com/300/300"
       subtitle="playlist"
     >
-      <Box
-        width="100%"
-        height="100%"
-        paddingX={"15px"}
-        bgColor={"rgba(0,0,0,0.1)"}
-      >
+      <Box width="100%" height="100%" paddingX="15px" bgColor="rgba(0,0,0,0.1)">
         <Flex padding="10px">
           <Text height="65px" fontSize="65px" color="lightgreen">
             <MdPlayCircleFilled />
@@ -56,9 +52,12 @@ const Playlist: FC<IPlaylist> = ({ playlist }) => {
             paddingX="15px"
             align="center"
             fontSize="40px"
-            color="lightgreen"
           >
-            <MdFavorite />
+            {playlist.userId === user?.id ? (
+              <MdFavorite color="lightgreen" />
+            ) : (
+              <MdFavoriteBorder />
+            )}
           </Text>
         </Flex>
       </Box>
@@ -68,26 +67,29 @@ const Playlist: FC<IPlaylist> = ({ playlist }) => {
 
 export const getServerSideProps = async ({ query, req }) => {
   const { id } = validateToken(req.cookies.SPOTIFY_CLONE_ACCESS_TOKEN);
-  const [playlist] = await prisma.playlist.findMany({
-    where: {
-      id: +query.id,
-      userId: id,
-    },
-    include: {
-      songs: {
-        include: {
-          artist: {
-            select: {
-              name: true,
-              id: true,
+  const [user, playlist] = await Promise.all([
+    prisma.user.findUnique({ where: { id: +id } }),
+    prisma.playlist.findMany({
+      where: {
+        id: +query.id,
+        userId: id,
+      },
+      include: {
+        songs: {
+          include: {
+            artist: {
+              select: {
+                name: true,
+                id: true,
+              },
             },
           },
         },
       },
-    },
-  });
+    }),
+  ]);
   return {
-    props: { playlist },
+    props: { playlist, user },
   };
 };
 
