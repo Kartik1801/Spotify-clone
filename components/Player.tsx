1f00465a750a3a04785689ad4ebd01804bc80653
kartik1801 <kartikdhawan1801@gmail.com>
2022-06-29 00:05:39 +0530

    up

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,63 +1,132 @@
 import {
-    ButtonGroup,
-    Box,
-    IconButton,
-    RangeSlider,
-    RangeSliderFilledTrack,
-    RangeSliderTrack,
-    RangeSliderThumb,
-    Center,
-    Flex,
-    Text
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangeSliderThumb,
+  Center,
+  Flex,
+  Text,
 } from "@chakra-ui/react";
 import ReactHowler from "react-howler";
 import { useEffect, useRef, useState } from "react";
 import {
-    MdShuffle,
-    MdSkipPrevious,
-    MdSkipNext,
-    MdPlayCircleFilled,
-    MdPauseCircleFilled,
-    MdOutlineRepeat
+  MdShuffle,
+  MdSkipPrevious,
+  MdSkipNext,
+  MdPlayCircleFilled,
+  MdPauseCircleFilled,
+  MdOutlineRepeat,
 } from "react-icons/md";
 import { useStoreActions } from "easy-peasy";
 
-const Player = () => {
-    return (
-        <Box width="100%">
-            <Box>
-                {/* <ReactHowler /> */}
-            </Box>
-            <Center color="gray.600" >
-                <ButtonGroup>
-                    <IconButton aria-label="shuffle" fontSize="16px" icon={< MdShuffle />} outline="none" variant="link" />
-                    <IconButton aria-label="previous" fontSize="20px" icon={< MdSkipPrevious />} outline="none" variant="link" />
-                    <IconButton aria-label="play" fontSize="42px" icon={< MdPlayCircleFilled />} outline="none" variant="link" />
-                    <IconButton aria-label="pause" fontSize="42px" icon={< MdPauseCircleFilled />} outline="none" variant="link" />
-                    <IconButton aria-label="next" fontSize="20px" icon={< MdSkipNext />} outline="none" variant="link" />
-                    <IconButton aria-label="repeat" fontSize="16px" icon={< MdOutlineRepeat />} outline="none" variant="link" />
-                </ButtonGroup>
-            </Center>
-            <Box color="gray.600" >
-                <Flex justify="center" align="center">
-                    <Box width="10%">
-                        <Text fontSize="xs">1:21</Text>
-                    </Box>
-                    <Box width="80%">
-                        <RangeSlider aria-label={['min', 'max']} step={"0.1"} min={0} max={321}>
-                            <RangeSliderTrack bg="gray.800">
-                                <RangeSliderFilledTrack bg="gray.600" ></RangeSliderFilledTrack>
-                            </RangeSliderTrack>
-                            <RangeSliderThumb index={0}></RangeSliderThumb>
-                        </RangeSlider>
-                    </Box>
-                    <Box width="10%" textAlign="right">
-                        <Text fontSize="xs" >3:21</Text>
-                    </Box>
-                </Flex>
-            </Box>
-        </Box>
-    )
-}
+const Player = ({ songs, activeSong }) => {
+  const [playing, setPlaying] = useState(true);
+  const [index, setIndex] = useState(0);
+  const [seek, setSeek] = useState(0.0);
+  const [repeat, setRepeat] = useState(false);
+  const [shuffle, setShuffle] = useState(false);
+  const [duration, setDuration] = useState(0.0);
+
+  const setPlayState = (value) => {
+    setPlaying(value);
+  };
+  const toogleShuffleState = () => {
+    setShuffle(!shuffle);
+  };
+  const toogleRepeatState = () => {
+    setRepeat(!repeat);
+  };
+
+  return (
+    <Box width="100%">
+      <Box>{/* <ReactHowler playing={playing} src={activeSong?.url} /> */}</Box>
+      <Center color="gray.600">
+        <ButtonGroup>
+          <IconButton
+            aria-label="shuffle"
+            fontSize="20px"
+            icon={<MdShuffle />}
+            color={shuffle ? "#1db954" : "gray.600"}
+            outline="none"
+            onClick={() => {
+              toogleShuffleState();
+            }}
+            variant="link"
+          />
+          <IconButton
+            aria-label="previous"
+            fontSize="20px"
+            icon={<MdSkipPrevious />}
+            outline="none"
+            variant="link"
+          />
+          {playing ? (
+            <IconButton
+              aria-label="pause"
+              fontSize="42px"
+              icon={<MdPauseCircleFilled />}
+              onClick={() => setPlayState(false)}
+              outline="none"
+              variant="link"
+            />
+          ) : (
+            <IconButton
+              aria-label="play"
+              fontSize="42px"
+              icon={<MdPlayCircleFilled />}
+              onClick={() => setPlayState(true)}
+              outline="none"
+              variant="link"
+            />
+          )}
+          <IconButton
+            aria-label="next"
+            fontSize="20px"
+            icon={<MdSkipNext />}
+            outline="none"
+            variant="link"
+          />
+          <IconButton
+            aria-label="repeat"
+            fontSize="20px"
+            icon={<MdOutlineRepeat />}
+            color={repeat ? "#1db954" : "gray.600"}
+            onClick={() => {
+              toogleRepeatState();
+            }}
+            outline="none"
+            variant="link"
+          />
+        </ButtonGroup>
+      </Center>
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="xs">1:21</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={["min", "max"]}
+              step={0.1}
+              min={0}
+              max={321}
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs">3:21</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
 
 export default Player;
--- components/PlayerBar.tsx
+++ components/PlayerBar.tsx
@@ -1,32 +1,31 @@
-import {
-    Box, Flex, Text
-} from "@chakra-ui/layout"
+import { Box, Flex, Text } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/react";
 import Player from "./Player";
 
 const PlayerBar = () => {
-    return (
-        <Box height="100px" width="100vw" bg="gray.900" padding="10px">
-            <Flex align="center" >
-                <Flex direction="row" width="30%">
-                    <Box
-                        paddingY="20px">
-                        <Image
-                            verticalAlign="center"
-                            height="44px"
-                            src={`https://picsum.photos/400?}`}
-                            boxShadow="dark-lg"
-                        />
-                    </Box>
-                    <Box paddingY="23px" paddingX="10px" color="white">
-                        <Text fontSize="large">SongName</Text>
-                        <Text fontSize="sm">Artist</Text>
-                    </Box>
-                </Flex>
-                <Box width="40%"><Player></Player></Box>
-            </Flex>
+  return (
+    <Box height="100px" width="100vw" bg="gray.900" padding="10px">
+      <Flex align="center">
+        <Flex direction="row" width="30%">
+          <Box paddingY="20px">
+            <Image
+              verticalAlign="center"
+              height="44px"
+              src="https://picsum.photos/400?}"
+              boxShadow="dark-lg"
+            />
+          </Box>
+          <Box paddingY="23px" paddingX="10px" color="white">
+            <Text fontSize="large">SongName</Text>
+            <Text fontSize="sm">Artist</Text>
+          </Box>
+        </Flex>
+        <Box width="40%">
+          <Player songs={undefined} activeSong={undefined} />
         </Box>
-    )
-}
+      </Flex>
+    </Box>
+  );
+};
 
 export default PlayerBar;
