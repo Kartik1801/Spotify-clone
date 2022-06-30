fb987a0ea5238cd42cb4149f4538a407cea21373
kartik1801 <kartikdhawan1801@gmail.com>
2022-06-30 23:55:27 +0530

    up

--- components/PlayerBar.tsx
+++ components/PlayerBar.tsx
@@ -1,27 +1,32 @@
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/react";
+import { useStoreState } from "easy-peasy";
 import Player from "./Player";
 
 const PlayerBar = () => {
+  const songs = useStoreState((state: any) => state.activeSongs);
+  const activeSong = useStoreState((state: any) => state.activeSong);
   return (
     <Box height="100px" width="100vw" bg="gray.900" padding="10px">
       <Flex align="center">
-        <Flex direction="row" width="30%">
-          <Box paddingY="20px">
-            <Image
-              verticalAlign="center"
-              height="44px"
-              src="https://picsum.photos/400?}"
-              boxShadow="dark-lg"
-            />
-          </Box>
-          <Box paddingY="23px" paddingX="10px" color="white">
-            <Text fontSize="large">SongName</Text>
-            <Text fontSize="sm">Artist</Text>
-          </Box>
-        </Flex>
+        {activeSong ? (
+          <Flex direction="row" width="30%">
+            <Box paddingY="20px">
+              <Image
+                verticalAlign="center"
+                height="44px"
+                src="https://picsum.photos/400?}"
+                boxShadow="dark-lg"
+              />
+            </Box>
+            <Box paddingY="23px" paddingX="10px" color="white">
+              <Text fontSize="large">{activeSong.name}</Text>
+              <Text fontSize="sm">{activeSong.artist.name}</Text>
+            </Box>
+          </Flex>
+        ) : null}
         <Box width="40%">
-          <Player songs={undefined} activeSong={undefined} />
+          {activeSong ? <Player songs={songs} activeSong={activeSong} /> : null}
         </Box>
       </Flex>
     </Box>
